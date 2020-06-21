d3fddb8bb8c70e1b86e573ee283a63a27fc917fa
evan <0x4861@beeglelabs.com>
2020-06-21 22:31:13 +0900

    [Mod] Row and Column

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -11,20 +11,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-              height: 100.0,
-              width: 100.0,
-              //margin: EdgeInsets.all(20.0),
-              //margin: EdgeInsets.symmetric(vertical:50.0, horizontal: 20.0),
-              //margin: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-              margin: EdgeInsets.only(left:30.0), // outside of widget
-              padding: EdgeInsets.all(20.0),      // inside of widget
-              color: Colors.white,
-              child: Text('Hello')
+          child: Column(
+            //verticalDirection: VerticalDirection.down,
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween, // 중앙 정렬
+            //crossAxisAlignment: CrossAxisAlignment.end, // 위젯의 끝 라인을 맞춰서 정렬
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                    height: 100.0,
+                    color: Colors.white,
+                    child: Text('Container 1')
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    height: 100.0,
+                    color: Colors.blue,
+                    child: Text('Container 2')
+                ),
+                Container(
+                    height: 100.0,
+                    color: Colors.red,
+                    child: Text('Container 3')
+                )
+              ],
           ),
         ),
       ),
     );
   }
 }
-
